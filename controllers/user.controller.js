41f609652c708622a067220101cc43fd6d5e0e59
khushi-khushwa <khushikhushwah70@gmail.com>
2024-09-21 19:42:27 +0530

    origin mian

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -109,8 +109,9 @@ export const logout = async (req, res) => {
 export const updateProfile = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, bio, skills } = req.body;
-        
-    
+        console.log( fullname, email, phoneNumber, bio, skills);
+    const file= req.file;
+
         let skillsArray;
         if(skills){
             skillsArray = skills.split(",");
