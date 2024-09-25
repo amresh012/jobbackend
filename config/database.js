3e7dbf67b75e3e0644ddc720e7a7df1249984160
khushi-khushwa <khushikhushwah70@gmail.com>
2024-09-25 14:13:21 +0530

    originmain

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DATABASE_URL, {
-            useNewUrlParser: true,
+           
             useUnifiedTopology: true,
         });
         console.log('MongoDB connected successfully');
